01753fd8a964c9d9ef429bd04bb687386463140a
tarun <tarunmedisetti13@gmail.com>
2025-08-10 21:17:36 +0530

    Second Commit

--- server.js
+++ server.js
@@ -23,12 +23,36 @@ const User = mongoose.model('User', sample_dbSchema);
 //function that inserts one document into collection
 async function insertUser() {
     const user = new User({
-        name: "Tarun",
-        email: "tharunmedisetti850@gmail.com",
-        password: "Dcme$265"
+        name: "Ashok",
+        email: "Ashok@gmail.com",
+        password: "Ashok12345"
     });
     const res = await user.save();
     console.log(res);
 }
 
-insertUser();
+//function that return Email is present in db or not
+async function CheckEmailPresence(email) {
+    const res = await User.findOne({ email: email });
+    return res;
+}
+
+async function UpdateUserByEmail() {
+    const email = "tharunmedisetti850@gmail.com";
+    const res = await CheckEmailPresence(email);
+    if (res) {
+        const res = await User.findOneAndUpdate(
+            { email: email },//filter
+            { $set: { name: "Tarun Medisetti" } },
+            { new: true } //return updated doc
+        );
+        console.log(res);
+    }
+    else {
+        console.log("Email Not found in DB");
+    }
+}
+
+//function that updates the document
+//insertUser();
+UpdateUserByEmail();
